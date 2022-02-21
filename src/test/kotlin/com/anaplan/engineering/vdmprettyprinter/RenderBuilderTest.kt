4f076e5761df9940364a755c4f0369b6d7524f48
Alessandro Pezzoni <alessandro.pezzoni@anaplan.com>
2022-02-21 14:18:07 +0000

    Fixed mockito dependency

--- src/test/kotlin/com/anaplan/engineering/vdmprettyprinter/RenderBuilderTest.kt
+++ src/test/kotlin/com/anaplan/engineering/vdmprettyprinter/RenderBuilderTest.kt
@@ -21,9 +21,9 @@
  */
 package com.anaplan.engineering.vdmprettyprinter
 
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.overture.ast.node.INode
